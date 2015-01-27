8eff00b69d4ddc6f7f0372d2745d27c43d3f2ccd
Nolimet <nolimet@stamdata.nl>
2015-01-27 16:26:10 +0100

    minor tweaks

--- Assets/Scripts/Player/Playermovement.cs
+++ Assets/Scripts/Player/Playermovement.cs
@@ -4,43 +4,42 @@ using System.Collections;
 
 
 public class PlayerMovement : MonoBehaviour
-	
 {
-	private Vector3 movementVector;
-	private CharacterController characterController;
-	private float movementSpeed = 4;
-	private float jumpPower = 15;
-	private float gravity = 40;
+    private Vector3 move;
+    private CharacterController characterController;
+    private float movementSpeed = 4;
+    private float jumpPower = 15;
+    private float gravity = 40;
 
-	
-	void Start()
-	{
-		characterController = GetComponent<CharacterController>();
-	}
-	
-	
-	void Update()
-	{
-		movementVector.x = Input.GetAxis ("LeftJoystickX+") * movementSpeed;
-		movementVector.x = Input.GetAxis ("LeftJoystickX-") * movementSpeed;
-		movementVector.z = Input.GetAxis ("LeftJoystickY+") * movementSpeed;
-		movementVector.z = Input.GetAxis ("LeftJoystickY-") * movementSpeed;
 
-		if(characterController.isGrounded)
-		{
-			movementVector.y = 0;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
-			if(Input.GetButtonDown("A"))
-			{
-				movementVector.y = jumpPower;
-			}
-		}
-		
-		
-		movementVector.y -= gravity * Time.deltaTime;
-		
-		characterController.Move(movementVector * Time.deltaTime);
-		
-	}
-	
+
+    void Update()
+    {
+        move.x = Input.GetAxis("LeftJoystickX+") * movementSpeed;
+        move.x = Input.GetAxis("LeftJoystickX-") * movementSpeed;
+        move.z = Input.GetAxis("LeftJoystickY+") * movementSpeed;
+        move.z = Input.GetAxis("LeftJoystickY-") * movementSpeed;
+
+        if (characterController.isGrounded)
+        {
+            move.y = 0;
+
+            if (Input.GetButtonDown("A"))
+            {
+                move.y = jumpPower;
+            }
+        }
+
+
+        move.y -= gravity * Time.deltaTime;
+
+        characterController.Move(move * Time.deltaTime);
+
+    }
+
 }
