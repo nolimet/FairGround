b9749d59e2cdaa35d7da4f773f388665ac4d10a5
Nolimet <nolimet@stamdata.nl>
2015-01-27 14:31:37 +0100

    Added a sound trap a a sound

--- Assets/Scripts/DumbMove.cs
+++ Assets/Scripts/DumbMove.cs
@@ -5,11 +5,13 @@ public class DumbMove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        Debug.LogWarning("I am not needed");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetAxis(Axis.Horizontal) != 0)
+            transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis(Axis.Horizontal));
+
 	}
 }
--- Assets/Scripts/Traps/SoundTrap.cs
+++ Assets/Scripts/Traps/SoundTrap.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(AudioSource))]
 public class SoundTrap : MonoBehaviour {
@@ -14,23 +14,25 @@ public class SoundTrap : MonoBehaviour {
     {
         canPlay = true;
         audio.minDistance = audio.maxDistance - 100f;
-        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+       // rigidbody.constraints = RigidbodyConstraints.FreezeAll;
         GetComponent<BoxCollider>().isTrigger = true;
     }
-	void OnTriggerEnter(Collider other){
+    void OnTriggerEnter(Collider other)
+    {
 
         if (canPlay &&other.tag == "Player")
         {
             canPlay = false;
             audio.PlayOneShot(sound);
             StartCoroutine(coolDown(3f));
-            print("trigger sound");
         }
+
     }
 
     IEnumerator coolDown(float t)
     {
         yield return new WaitForSeconds(t);
         canPlay = true;
+        print("done wating");
     }
 }
