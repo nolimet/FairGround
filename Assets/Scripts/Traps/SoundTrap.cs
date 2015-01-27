3ce901c2a25ba0aa312138f2a9a071d6da781d87
Nolimet <nolimet@stamdata.nl>
2015-01-27 14:06:40 +0100

    added a model

---
+++ Assets/Scripts/Traps/SoundTrap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
+public class SoundTrap : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip sound;
+    bool canPlay;
+
+    void Start()
+    {
+        canPlay = true;
+        audio.minDistance = audio.maxDistance - 100f;
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+	void OnTriggerEnter(Collider other){
+
+        if (canPlay &&other.tag == "Player")
+        {
+            canPlay = false;
+            audio.PlayOneShot(sound);
+            StartCoroutine(coolDown(3f));
+            print("trigger sound");
+        }
+    }
+
+    IEnumerator coolDown(float t)
+    {
+        yield return new WaitForSeconds(t);
+        canPlay = true;
+    }
+}
