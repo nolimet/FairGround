e7046ff3627f5da085f3f91f0a9ab018d394a189
Nolimet <nolimet@stamdata.nl>
2015-01-27 15:53:04 +0100

    added new fbx and upgraded lights

---
+++ Assets/Scripts/util/FlickerLight.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class FlickerLight : MonoBehaviour
+{
+
+    [SerializeField]
+    float min, max;
+
+    [SerializeField]
+    Light[] Lights;
+    [SerializeField]
+    bool autoMap;
+    [SerializeField]
+    string LightTag;
+
+    void Start()
+    {
+        StartCoroutine(flickering());
+    }
+
+    IEnumerator flickering()
+    {
+        float a, b, c,d = max;
+        int length;
+        bool add = false;
+        while (true)
+        {
+            length = (int)Random.Range(20, 120);
+            a = Random.Range(min, max * 0.3f);
+            b = Random.Range(max * 0.5f, max);
+            c = b - a;
+            for (int i = 0; i < length; i++)
+            {
+                if (d >= b)
+                    add = false;
+                if (d <= a)
+                    add = true;
+
+                if (add)
+                    d += c * Time.deltaTime;
+                else
+                    d -= c * Time.deltaTime;
+                foreach(Light l in Lights)
+                light.intensity=d;
+                yield return new WaitForEndOfFrame();
+
+            }
+        }
+    }
+
+    void AutoMapLights()
+    {
+        System.Collections.Generic.List<Light> l = new System.Collections.Generic.List<Light>();
+
+        GameObject[] games = GameObject.FindGameObjectsWithTag(LightTag);
+
+        foreach (GameObject g in games)
+        {
+            l.Add(g.GetComponent<Light>());
+        }
+
+        Lights = l.ToArray();
+    }
+}
